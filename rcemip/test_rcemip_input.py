282b2a8bc8e665f3c068153df6e8e95a5726eb3d
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2019-05-16 15:49:24 +0200

    Wrong lev size leading to crashes in RRTMGP.

--- rcemip/test_rcemip_input.py
+++ rcemip/test_rcemip_input.py
@@ -8,19 +8,20 @@ nc_file = nc.Dataset("test_rcemip_input.nc", mode="w", datamodel="NETCDF4", clob
 # Radiation profiles.
 p0 = 101480.
 dz = 500.
-zsize = 100.e3
-z  = np.arange(dz/2, zsize, dz)
-zh = np.arange(   0, zsize, dz)
+z_top = 60.e3
+z  = np.arange(dz/2, z_top, dz)
+zh = np.arange(   0, z_top, dz)
+zh = np.append(zh, z_top)
 
 def calc_p_q_T(z):
     q_0 = 0.01864 # for 300 K SST.
     z_q1 = 4.0e3
     z_q2 = 7.5e3
     z_t = 15.e3
-    q_t = 1.e-11
+    q_t = 1.e-14
 
     q = q_0 * np.exp(-z/z_q1) * np.exp(-(z/z_q2)**2)
-    i_above_zt = np.where(z>z_t)
+    i_above_zt = np.where(z > z_t)
     q[i_above_zt] = q_t
     
     T_0 = 300.
@@ -33,7 +34,7 @@ def calc_p_q_T(z):
     
     g = 9.79764
     Rd = 287.04
-    p = p0 * ((Tv_0 - gamma*z) / Tv_0)**(g/(Rd*gamma))
+    p = p0 * (Tv / Tv_0)**(g/(Rd*gamma))
     
     p_tmp = p0 * (Tv_t/Tv_0)**(g/(Rd*gamma)) \
           * np.exp( -( (g*(z-z_t)) / (Rd*Tv_t) ) )
@@ -55,31 +56,30 @@ g3 = 11.3515
 p_hpa = p_lay/100.
 o3 = g1 * p_hpa**g2 * np.exp(-p_hpa/g3) * 1e-6
 
-nc_file.createDimension("p_lay", p_lay.size)
-nc_file.createDimension("p_lev", p_lay.size)
-nc_p_lay = nc_file.createVariable("p_lay", float_type, ("p_lay"))
-nc_p_lev = nc_file.createVariable("p_lev", float_type, ("p_lev"))
+nc_group_rad = nc_file.createGroup("radiation")
 
+nc_group_rad.createDimension("lay", p_lay.size)
+nc_group_rad.createDimension("lev", p_lev.size)
+nc_p_lay = nc_group_rad.createVariable("p_lay", float_type, ("lay"))
+nc_p_lev = nc_group_rad.createVariable("p_lev", float_type, ("lev"))
 nc_p_lay[:] = p_lay[:]
 nc_p_lev[:] = p_lev[:]
 
-nc_group_rad = nc_file.createGroup("radiation")
-
-nc_T_lay = nc_group_rad.createVariable("T_lay", float_type, ("p_lay"))
-nc_T_lev = nc_group_rad.createVariable("T_lev", float_type, ("p_lev"))
+nc_T_lay = nc_group_rad.createVariable("t_lay", float_type, ("lay"))
+nc_T_lev = nc_group_rad.createVariable("t_lev", float_type, ("lev"))
 nc_T_lay[:] = T_lay[:]
 nc_T_lev[:] = T_lev[:]
 
-nc_CO2 = nc_group_rad.createVariable("co2", float_type, ("p_lay"))
-nc_CH4 = nc_group_rad.createVariable("ch4", float_type, ("p_lay"))
-nc_N2O = nc_group_rad.createVariable("n2o", float_type, ("p_lay"))
-nc_O3  = nc_group_rad.createVariable("o3" , float_type, ("p_lay"))
-nc_H2O = nc_group_rad.createVariable("h2o", float_type, ("p_lay"))
+nc_CO2 = nc_group_rad.createVariable("co2", float_type, ("lay"))
+nc_CH4 = nc_group_rad.createVariable("ch4", float_type, ("lay"))
+nc_N2O = nc_group_rad.createVariable("n2o", float_type, ("lay"))
+nc_O3  = nc_group_rad.createVariable("o3" , float_type, ("lay"))
+nc_H2O = nc_group_rad.createVariable("h2o", float_type, ("lay"))
 
 nc_CO2[:] = co2
 nc_CH4[:] = ch4
 nc_N2O[:] = n2o
 nc_O3 [:] = o3 [:]
-nc_H2O[:] = h2o[:]
+nc_H2O[:] = h2o[:] / 100000
 
 nc_file.close()
