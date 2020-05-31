19740e8c6e6548f2b27ed0bdf864b8c0fed11f28
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2020-05-31 08:29:41 +0200

    Added config file for GCC on macbook.

---
+++ config/macbook_brew_gcc.cmake
@@ -0,0 +1,29 @@
+# MacBook using GCC compiler from homebrew
+if(USEMPI) 
+  set(ENV{CC}  mpicc ) # C compiler for parallel build
+  set(ENV{CXX} mpicxx) # C++ compiler for parallel build
+  set(ENV{FC}  mpif90) # Fortran compiler for parallel build
+else()
+  set(ENV{CC}  gcc-9)      # C compiler for serial build
+  set(ENV{CXX} g++-9 )     # C++ compiler for serial build
+  set(ENV{FC}  gfortran-9) # Fortran compiler for parallel build
+endif()
+
+set(GNU_SED "gsed")
+
+set(USER_CXX_FLAGS "-std=c++14 -DBOOL_TYPE=\"signed char\"")
+set(USER_CXX_FLAGS_RELEASE "-DNDEBUG -O3 -march=native")
+set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+set(USER_FC_FLAGS "-std=f2003 -fdefault-real-8 -fdefault-double-8 -fPIC -ffixed-line-length-none -fno-range-check -DUSE_CBOOL")
+set(USER_FC_FLAGS_RELEASE "-DNDEBUG -O3 -march=native")
+set(USER_FC_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+set(NETCDF_INCLUDE_DIR "/usr/local/include")
+set(NETCDF_LIB_C       "/usr/local/lib/libnetcdf.dylib")
+set(HDF5_LIB_1         "/usr/local/lib/libhdf5.dylib")
+set(HDF5_LIB_2         "/usr/local/lib/libhdf5_hl.dylib")
+set(SZIP_LIB           "/usr/local/lib/libsz.dylib")
+set(LIBS ${NETCDF_LIB_CPP} ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
+set(INCLUDE_DIRS ${FFTW_INCLUDE_DIR} ${NETCDF_INCLUDE_DIR})
+
+add_definitions(-DRESTRICTKEYWORD=__restrict__)
