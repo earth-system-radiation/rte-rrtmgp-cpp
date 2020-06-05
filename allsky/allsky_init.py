8c7a0bdd65909e74e70e5cdaee26edd8bda4d6bc
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2020-06-05 10:15:24 +0200

    Running example, still errors in cloud optics.

--- allsky/allsky_init.py
+++ allsky/allsky_init.py
@@ -3,7 +3,7 @@ import netCDF4 as nc
 
 # Settings
 float_type = 'f8'
-ncol = 2
+ncol = 128
 
 band_lw = 16
 band_sw = 14
@@ -24,28 +24,27 @@ nc_p_lev = nc_file.createVariable('p_lev', float_type, ('lev', 'col'))
 nc_t_lay = nc_file.createVariable('t_lay', float_type, ('lay', 'col'))
 nc_t_lev = nc_file.createVariable('t_lev', float_type, ('lev', 'col'))
 
-nc_p_lay[:,:] = nc_file_garand.variables['p_lay'][:,:]
-nc_p_lev[:,:] = nc_file_garand.variables['p_lev'][:,:]
+nc_p_lay[:,:] = np.tile( nc_file_garand.variables['p_lay'][:,0][:,None], (1, ncol) )
+nc_p_lev[:,:] = np.tile( nc_file_garand.variables['p_lev'][:,0][:,None], (1, ncol) )
 
 # Make sure the top edge does not exceed the minimum tolerable pressure
 # of the coefficient files.
 nc_p_lev[:,:] = np.maximum(nc_p_lev[:,:], np.nextafter(1.005183574463, 1.))
 
-nc_t_lay[:,:] = nc_file_garand.variables['t_lay'][:,:]
-nc_t_lev[:,:] = nc_file_garand.variables['t_lev'][:,:]
+nc_t_lay[:,:] = np.tile( nc_file_garand.variables['t_lay'][:,0][:,None], (1, ncol) )
+nc_t_lev[:,:] = np.tile( nc_file_garand.variables['t_lev'][:,0][:,None], (1, ncol) )
 
 nc_col_dry = nc_file.createVariable('col_dry', float_type, ('lay', 'col'))
-nc_col_dry[:,:] = nc_file_garand.variables['col_dry'][:,:]
+nc_col_dry[:,:] = np.tile( nc_file_garand.variables['col_dry'][:,0][:,None], (1, ncol) )
 
 nc_surface_emissivity = nc_file.createVariable('emis_sfc', float_type, ('col', 'band_lw'))
-nc_surface_temperature = nc_file.createVariable('t_sfc', float_type, 'col')
-
 nc_surface_emissivity[:,:] = 0.98
+
+nc_surface_temperature = nc_file.createVariable('t_sfc', float_type, 'col')
 nc_surface_temperature[:] = nc_t_lev[0,:]
 
 nc_surface_albedo_dir = nc_file.createVariable('sfc_alb_dir', float_type, ('col', 'band_sw'))
 nc_surface_albedo_dif = nc_file.createVariable('sfc_alb_dif', float_type, ('col', 'band_sw'))
-
 nc_surface_albedo_dir[:,:] = 0.06
 nc_surface_albedo_dif[:,:] = 0.06
 
@@ -62,14 +61,14 @@ nc_o2      = nc_file.createVariable('vmr_o2' , float_type, ('lay', 'col'))
 nc_n2      = nc_file.createVariable('vmr_n2' , float_type, ('lay', 'col'))
 
 # Profiles
-nc_h2o[:,:] = nc_file_garand.variables['vmr_h2o'][:,:]
-nc_co2[:,:] = nc_file_garand.variables['vmr_co2'][:,:]
-nc_o3 [:,:] = nc_file_garand.variables['vmr_o3' ][:,:]
-nc_n2o[:,:] = nc_file_garand.variables['vmr_n2o'][:,:]
-nc_co [:,:] = nc_file_garand.variables['vmr_co' ][:,:]
-nc_ch4[:,:] = nc_file_garand.variables['vmr_ch4'][:,:]
-nc_o2 [:,:] = nc_file_garand.variables['vmr_o2' ][:,:]
-nc_n2 [:,:] = nc_file_garand.variables['vmr_n2' ][:,:]
+nc_h2o[:,:] = np.tile( nc_file_garand.variables['vmr_h2o'][:,0][:,None], (1, ncol) )
+nc_co2[:,:] = np.tile( nc_file_garand.variables['vmr_co2'][:,0][:,None], (1, ncol) )
+nc_o3 [:,:] = np.tile( nc_file_garand.variables['vmr_o3' ][:,0][:,None], (1, ncol) )
+nc_n2o[:,:] = np.tile( nc_file_garand.variables['vmr_n2o'][:,0][:,None], (1, ncol) )
+nc_co [:,:] = np.tile( nc_file_garand.variables['vmr_co' ][:,0][:,None], (1, ncol) )
+nc_ch4[:,:] = np.tile( nc_file_garand.variables['vmr_ch4'][:,0][:,None], (1, ncol) )
+nc_o2 [:,:] = np.tile( nc_file_garand.variables['vmr_o2' ][:,0][:,None], (1, ncol) )
+nc_n2 [:,:] = np.tile( nc_file_garand.variables['vmr_n2' ][:,0][:,None], (1, ncol) )
 
 # Clouds
 nc_lwp = nc_file.createVariable('lwp', float_type, ('lay', 'col'))
@@ -77,8 +76,6 @@ nc_iwp = nc_file.createVariable('iwp', float_type, ('lay', 'col'))
 nc_rel = nc_file.createVariable('rel', float_type, ('lay', 'col'))
 nc_rei = nc_file.createVariable('rei', float_type, ('lay', 'col'))
 
-#rel_val = 0.5 * (cloud_optics%get_min_radius_liq() + cloud_optics%get_max_radius_liq())
-#rei_val = 0.5 * (cloud_optics%get_min_radius_ice() + cloud_optics%get_max_radius_ice())
 min_rel, max_rel = 2.5, 21.5
 min_rei, max_rei = 10., 180.
 
@@ -90,7 +87,7 @@ cloud_mask = np.zeros((nlay, ncol))
 
 for ilay in range(nlay):
     for icol in range(ncol):
-        cloud_mask[ilay, icol] = (nc_p_lay[ilay, icol] > 1.e4) and (nc_p_lay[ilay, icol] < 9.e4) and (icol%3 != 0)
+        cloud_mask[ilay, icol] = (nc_p_lay[ilay, icol] > 1.e4) and (nc_p_lay[ilay, icol] < 9.e4) and ((icol+1)%3 != 0)
         nc_lwp[ilay, icol] = 10. if (cloud_mask[ilay, icol] and (nc_t_lay[ilay, icol] > 263.)) else 0.
         nc_iwp[ilay, icol] = 10. if (cloud_mask[ilay, icol] and (nc_t_lay[ilay, icol] < 273.)) else 0.
         nc_rel[ilay, icol] = rel_val if cloud_mask[ilay, icol] else 0.
--- allsky/allsky_plot.py
+++ allsky/allsky_plot.py
@@ -6,7 +6,10 @@ nc_file_run = nc.Dataset('rrtmgp-allsky.nc', 'r')
 nc_file_ref = nc.Dataset('ref/rrtmgp-allsky.nc', 'r')
 
 sw_flux_dn_run = nc_file_run.variables['sw_flux_dn'][:,:]
-sw_flux_dn_ref = nc_file_ref.variables['sw_flux_dn'][:,:2]
+sw_flux_dn_ref = nc_file_ref.variables['sw_flux_dn'][:,:]
+
+sw_flux_dir_run = nc_file_run.variables['sw_flux_dir'][:,:]
+sw_flux_dir_ref = nc_file_ref.variables['sw_flux_dir'][:,:]
 
 plt.figure()
 plt.subplot(311)
@@ -19,4 +22,20 @@ plt.colorbar()
 plt.subplot(313)
 plt.pcolormesh(sw_flux_dn_run - sw_flux_dn_ref)
 plt.colorbar()
+plt.tight_layout()
+
+plt.figure()
+plt.subplot(311)
+plt.pcolormesh(sw_flux_dir_run)
+plt.colorbar()
+plt.title('sw_flux_dir')
+plt.subplot(312)
+plt.pcolormesh(sw_flux_dir_ref)
+plt.colorbar()
+plt.subplot(313)
+plt.pcolormesh(sw_flux_dir_run - sw_flux_dir_ref)
+plt.colorbar()
+plt.tight_layout()
+
+
 plt.show()
