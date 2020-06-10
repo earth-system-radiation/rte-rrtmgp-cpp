efa9e92dedcd7894eac619de7d160e9ab38526e9
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2020-06-10 22:06:02 +0200

    Run the cloud optics in the allsky test.

--- allsky/allsky_run.py
+++ allsky/allsky_run.py
@@ -8,7 +8,7 @@ def remove(filename):
         pass
 
 remove('rte_rrtmgp_output.nc')
-subprocess.run(['./test_rte_rrtmgp'])
+subprocess.run(['./test_rte_rrtmgp', '--cloud-optics'])
 
 remove('rrtmgp-allsky.nc')
 subprocess.run('ncrename -h -v sw_flux_dn_dir,sw_flux_dir -d col,col_flx rte_rrtmgp_output.nc rrtmgp-allsky.nc'.split())
