16500cdd82a39693550c9585e5d792793b2c66f8
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2020-09-27 20:42:10 +0200

    Compiling code, needs kernel now for col_dry computation.

--- src/Gas_optics_rrtmgp.cu
+++ src/Gas_optics_rrtmgp.cu
@@ -1,3 +1,27 @@
+/*
+ * This file is part of a C++ interface to the Radiative Transfer for Energetics (RTE)
+ * and Rapid Radiative Transfer Model for GCM applications Parallel (RRTMGP).
+ *
+ * The original code is found at https://github.com/earth-system-radiation/rte-rrtmgp.
+ *
+ * Contacts: Robert Pincus and Eli Mlawer
+ * email: rrtmgp@aer.com
+ *
+ * Copyright 2015-2020,  Atmospheric and Environmental Research and
+ * Regents of the University of Colorado.  All right reserved.
+ *
+ * This C++ interface can be downloaded from https://github.com/earth-system-radiation/rte-rrtmgp-cpp
+ *
+ * Contact: Chiel van Heerwaarden
+ * email: chiel.vanheerwaarden@wur.nl
+ *
+ * Copyright 2020, Wageningen University & Research.
+ *
+ * Use and duplication is permitted under the terms of the
+ * BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
 #include "Gas_optics_rrtmgp.h"
 
 // Calculate the molecules of dry air.
@@ -16,7 +40,8 @@ void Gas_optics_rrtmgp<TF>::get_col_dry_gpu(
     Array_gpu<TF,2> delta_plev({col_dry.dim(1), col_dry.dim(2)});
     Array_gpu<TF,2> m_air     ({col_dry.dim(1), col_dry.dim(2)});
 
-    // CvH: the code below should be replaced by a kernel, this is really slow.
+    // CvH: the code below should be replaced by a kernel
+    /*
     for (int ilay=1; ilay<=col_dry.dim(2); ++ilay)
         for (int icol=1; icol<=col_dry.dim(1); ++icol)
             delta_plev({icol, ilay}) = std::abs(plev({icol, ilay}) - plev({icol, ilay+1}));
@@ -31,4 +56,11 @@ void Gas_optics_rrtmgp<TF>::get_col_dry_gpu(
             col_dry({icol, ilay}) = TF(10.) * delta_plev({icol, ilay}) * avogad / (TF(1000.)*m_air({icol, ilay})*TF(100.)*g0);
             col_dry({icol, ilay}) /= (TF(1.) + vmr_h2o({icol, ilay}));
         }
+        */
 }
+
+#ifdef FLOAT_SINGLE_RRTMGP
+template class Gas_optics_rrtmgp<float>;
+#else
+template class Gas_optics_rrtmgp<double>;
+#endif
