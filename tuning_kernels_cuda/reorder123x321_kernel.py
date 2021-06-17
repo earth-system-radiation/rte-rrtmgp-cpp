8bb3500c47a9c31c6f5a616674e70775c0623d95
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2021-06-17 11:09:58 +0200

    Fix JSON file and tuning parameters.

--- tuning_kernels_cuda/reorder123x321_kernel.py
+++ tuning_kernels_cuda/reorder123x321_kernel.py
@@ -43,15 +43,15 @@ def run_and_test(params: dict):
 # Tuning the kernel
 def tune():
     tune_params = dict()
-    tune_params['block_size_x'] = [1,2,4,8,16,24,32]
-    tune_params['block_size_y'] = [1,2,4,8,16,24,32]
-    tune_params['block_size_z'] = [1,2,4,8,16,24,32]
+    tune_params['block_size_x'] = [2**i for i in range(0, 11)]
+    tune_params['block_size_y'] = [2**i for i in range(0, 11)]
+    tune_params['block_size_z'] = [2**i for i in range(0, 7)]
 
     result, env = kt.tune_kernel(
             kernel_name, kernel_string, problem_size,
             args, tune_params, compiler_options=cp)
 
-    with open('reorder123x321_kernel.json', 'w') as fp:
+    with open('timings_reorder123x321_kernel.json', 'w') as fp:
         json.dump(result, fp)
 
 
