88be166f9663392a1e085b1bdafb7cbb5d762b47
Bart van Stratum <bart@vanstratum.com>
2021-06-21 14:28:13 +0000

    Cleanup `compute_tau_minor` kernels

--- src_kernels_cuda/gas_optics_kernel_launchers.cu
+++ src_kernels_cuda/gas_optics_kernel_launchers.cu
@@ -245,7 +245,7 @@ namespace rrtmgp_kernel_launcher_cuda
         dim3 grid_gpu_min_1(grid_col_min_1, grid_lay_min_1);
         dim3 block_gpu_min_1(block_col_min_1, block_lay_min_1);
 
-        compute_tau_minor_absorption_kernel_split<<<grid_gpu_min_1, block_gpu_min_1>>>(
+        compute_tau_minor_absorption_kernel<<<grid_gpu_min_1, block_gpu_min_1>>>(
                 ncol, nlay, ngpt,
                 ngas, nflav, ntemp, neta,
                 nscale_lower,
@@ -276,7 +276,7 @@ namespace rrtmgp_kernel_launcher_cuda
         dim3 grid_gpu_min_2(grid_col_min_2, grid_lay_min_2);
         dim3 block_gpu_min_2(block_col_min_2, block_lay_min_2);
 
-        compute_tau_minor_absorption_kernel_split<<<grid_gpu_min_2, block_gpu_min_2>>>(
+        compute_tau_minor_absorption_kernel<<<grid_gpu_min_2, block_gpu_min_2>>>(
                 ncol, nlay, ngpt,
                 ngas, nflav, ntemp, neta,
                 nscale_upper,
@@ -294,35 +294,6 @@ namespace rrtmgp_kernel_launcher_cuda
                 play.ptr(), tlay.ptr(), col_gas.ptr(),
                 fminor.ptr(), jeta.ptr(), jtemp.ptr(),
                 tropo.ptr(), tau.ptr(), tau_minor);
-
-
-//        const int block_col_min = 1;
-//        const int block_lay_min = 3;
-//
-//        const int grid_col_min  = ncol/block_col_min + (ncol%block_col_min > 0);
-//        const int grid_lay_min  = nlay/block_lay_min + (nlay%block_lay_min > 0);
-//
-//        dim3 grid_gpu_min(grid_col_min, grid_lay_min);
-//        dim3 block_gpu_min(block_col_min, block_lay_min);
-//
-//        compute_tau_minor_absorption_kernel<<<grid_gpu_min, block_gpu_min>>>(
-//                ncol, nlay, ngpt,
-//                ngas, nflav, ntemp, neta,
-//                nscale_lower, nscale_upper,
-//                nminorlower, nminorupper,
-//                nminorklower,nminorkupper,
-//                idx_h2o,
-//                gpoint_flavor.ptr(),
-//                kminor_lower.ptr(), kminor_upper.ptr(),
-//                minor_limits_gpt_lower.ptr(), minor_limits_gpt_upper.ptr(),
-//                minor_scales_with_density_lower.ptr(), minor_scales_with_density_upper.ptr(),
-//                scale_by_complement_lower.ptr(), scale_by_complement_upper.ptr(),
-//                idx_minor_lower.ptr(), idx_minor_upper.ptr(),
-//                idx_minor_scaling_lower.ptr(), idx_minor_scaling_upper.ptr(),
-//                kminor_start_lower.ptr(), kminor_start_upper.ptr(),
-//                play.ptr(), tlay.ptr(), col_gas.ptr(),
-//                fminor.ptr(), jeta.ptr(), jtemp.ptr(),
-//                tropo.ptr(), tau.ptr(), tau_minor);
 
         Tools_gpu::free_gpu(tau_major);
         Tools_gpu::free_gpu(tau_minor);
