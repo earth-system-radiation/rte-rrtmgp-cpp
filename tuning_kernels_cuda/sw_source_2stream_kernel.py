940fa7de1cf41663aee88830d750271d28c9add9
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2021-07-26 09:35:55 +0200

    Working on using common.

--- tuning_kernels_cuda/sw_source_2stream_kernel.py
+++ tuning_kernels_cuda/sw_source_2stream_kernel.py
@@ -4,10 +4,9 @@ import kernel_tuner as kt
 import numpy as np
 import argparse
 import json
-import os
-from collections import OrderedDict
+import common
 
-from common import *
+from collections import OrderedDict
 
 
 # Parse command line arguments
@@ -28,7 +27,8 @@ def run_and_test(params: dict):
     #call first reference kernel
     two_stream_result = kt.run_kernel(
             two_stream_ref_kernel_name, ref_kernels_src, two_stream_ref_problem_size,
-            two_stream_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=1, block_size_z=1), compiler_options=cp)
+            two_stream_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=1, block_size_z=1),
+            compiler_options=common.cp)
 
     #put the input from the 2stream reference kernel into the input of the source reference kernel
     source_kernel_args[4] = two_stream_result[-3]
@@ -37,12 +37,14 @@ def run_and_test(params: dict):
 
     source_ref_result = kt.run_kernel(
             source_ref_kernel_name, ref_kernels_src, ref_problem_size,
-            source_kernel_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=11), compiler_options=cp)
+            source_kernel_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=11),
+            compiler_options=common.cp)
 
     #call the fused kernel
     fused_result = kt.run_kernel(
             fused_kernel_name, kernels_src, ref_problem_size,
-            fused_kernel_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=16, block_size_y=16), compiler_options=cp)
+            fused_kernel_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=16, block_size_y=16),
+            compiler_options=common.cp)
 
     #test source outputs
     outputs = ["flux_dir", "source_sfc", "source_dn", "source_up"]
@@ -52,7 +54,7 @@ def run_and_test(params: dict):
         if okay:
             print(f'results for {o}: OKAY!')
         else:
-            print(f'results for {o}: NOT OKAY, {max_diff=}')
+            print(f'results for {o}: NOT OKAY, {max_diff}')
             print("Result:")
             print(fused_result[-1-i])
             print("Expected:")
@@ -66,7 +68,7 @@ def run_and_test(params: dict):
         if okay:
             print(f'results for {o}: OKAY!')
         else:
-            print(f'results for {o}: NOT OKAY, {max_diff=}')
+            print(f'results for {o}: NOT OKAY, {max_diff}')
             print("Result:")
             print(fused_result[-6-i])
             print("Expected:")
@@ -86,7 +88,8 @@ def tune_fused_kernel():
     #call first reference kernel
     two_stream_result = kt.run_kernel(
             two_stream_ref_kernel_name, ref_kernels_src, two_stream_ref_problem_size,
-            two_stream_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=1, block_size_z=1), compiler_options=cp)
+            two_stream_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=1, block_size_z=1),
+            compiler_options=common.cp)
 
     #put the input from the 2stream reference kernel into the input of the source reference kernel
     source_kernel_args[4] = two_stream_result[-3]
@@ -95,7 +98,8 @@ def tune_fused_kernel():
 
     source_ref_result = kt.run_kernel(
             source_ref_kernel_name, ref_kernels_src, ref_problem_size,
-            source_kernel_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=11), compiler_options=cp)
+            source_kernel_args, dict(RTE_RRTMGP_USE_CBOOL=1, block_size_x=64, block_size_y=11),
+            compiler_options=common.cp)
 
     answer = [None for _ in fused_kernel_args]
     answer[-1] = source_ref_result[-1] #flux_dir
@@ -109,9 +113,9 @@ def tune_fused_kernel():
     #tune the fused kernel
     fused_result = kt.tune_kernel(
             fused_kernel_name, kernels_src, ref_problem_size,
-            fused_kernel_args, tune_params, compiler_options=cp,
+            fused_kernel_args, tune_params, compiler_options=common.cp,
             answer=answer, atol=1e-14,
-            verbose=True, observers=[reg_observer], metrics=metrics, iterations=32)
+            verbose=True, observers=[common.reg_observer], metrics=common.metrics, iterations=32)
 
     with open('timings_sw_source_2stream_kernel.json', 'w') as fp:
         json.dump(fused_result, fp)
@@ -130,7 +134,7 @@ def tune_two_stream_ref():
     #ref 2stream_kernel is 883us on A100
     kt.tune_kernel(
             two_stream_ref_kernel_name, ref_kernels_src, two_stream_ref_problem_size,
-            two_stream_args, tune_params, compiler_options=cp)
+            two_stream_args, tune_params, compiler_options=common.cp)
 
 def tune_source_ref():
 
@@ -142,7 +146,7 @@ def tune_source_ref():
     #ref kernel is about 662us on A100
     kt.tune_kernel(
             source_ref_kernel_name, ref_kernels_src, ref_problem_size,
-            source_kernel_args, tune_params, compiler_options=cp)
+            source_kernel_args, tune_params, compiler_options=common.cp)
 
 
 
@@ -150,16 +154,15 @@ def tune_source_ref():
 if __name__ == '__main__':
     command_line = parse_command_line()
 
+    kernels_src = common.dir_name + '../src_kernels_cuda/rte_solver_kernels.cu'
+    ref_kernels_src = common.dir_name + 'reference_kernels/rte_solver_kernels.cu'
+
     # Settings
-    type_int = np.int32
-    type_float = np.float64
-    type_bool = np.int8  # = default without `RTE_RRTMGP_USE_CBOOL`
+    type_bool = np.int8
 
-    str_float = 'float' if type_float is np.float32 else 'double'
-
-    ncol = type_int(512)
-    nlay = type_int(140)
-    ngpt = type_int(224)
+    ncol = common.type_int(512)
+    nlay = common.type_int(140)
+    ngpt = common.type_int(224)
 
     top_at_1 = type_bool(1)
 
@@ -167,33 +170,33 @@ if __name__ == '__main__':
     flx_size = ncol * (nlay+1) * ngpt
     alb_size = ncol * ngpt
 
-    tau  = np.random.random(opt_size).astype(type_float)
-    ssa  = np.random.random(opt_size).astype(type_float)
-    g = np.random.random(opt_size).astype(type_float)
-    mu0 = np.random.random(ncol).astype(type_float)
+    tau  = np.random.random(opt_size).astype(common.type_float)
+    ssa  = np.random.random(opt_size).astype(common.type_float)
+    g = np.random.random(opt_size).astype(common.type_float)
+    mu0 = np.random.random(ncol).astype(common.type_float)
 
-    tmin = type_float(sys.float_info.min)
+    tmin = common.type_float(sys.float_info.min)
 
     # Input arrays; for this kernel, the values don't matter..
-    t_noscat = np.random.random(opt_size).astype(type_float)
-    flux_up  = np.random.random(flx_size).astype(type_float)
-    flux_dn  = np.random.random(flx_size).astype(type_float)
-    flux_dir = 1+np.random.random(flx_size).astype(type_float)
+    t_noscat = np.random.random(opt_size).astype(common.type_float)
+    flux_up  = np.random.random(flx_size).astype(common.type_float)
+    flux_dn  = np.random.random(flx_size).astype(common.type_float)
+    flux_dir = 1+np.random.random(flx_size).astype(common.type_float)
 
-    sfc_alb_dir = np.random.random(alb_size).astype(type_float)
-    sfc_alb_dif = np.random.random(alb_size).astype(type_float)
+    sfc_alb_dir = np.random.random(alb_size).astype(common.type_float)
+    sfc_alb_dif = np.random.random(alb_size).astype(common.type_float)
 
     # Output arrays
-    r_dif = np.zeros(opt_size, dtype=type_float);
-    t_dif = np.zeros(opt_size, dtype=type_float);
-    r_dir = np.zeros(opt_size, dtype=type_float);
-    t_dir = np.zeros(opt_size, dtype=type_float);
-    source_up = np.zeros(opt_size, dtype=type_float);
-    source_dn = np.zeros(opt_size, dtype=type_float);
-    source_sfc = np.zeros(alb_size, dtype=type_float);
-    albedo = np.zeros(flx_size, dtype=type_float);
-    src = np.zeros(flx_size, dtype=type_float);
-    denom = np.zeros(opt_size, dtype=type_float);
+    r_dif = np.zeros(opt_size, dtype=common.type_float)
+    t_dif = np.zeros(opt_size, dtype=common.type_float)
+    r_dir = np.zeros(opt_size, dtype=common.type_float)
+    t_dir = np.zeros(opt_size, dtype=common.type_float)
+    source_up = np.zeros(opt_size, dtype=common.type_float)
+    source_dn = np.zeros(opt_size, dtype=common.type_float)
+    source_sfc = np.zeros(alb_size, dtype=common.type_float)
+    albedo = np.zeros(flx_size, dtype=common.type_float)
+    src = np.zeros(flx_size, dtype=common.type_float)
+    denom = np.zeros(opt_size, dtype=common.type_float)
 
     two_stream_args = [ncol, nlay, ngpt, tmin,
             tau, ssa,
@@ -211,14 +214,14 @@ if __name__ == '__main__':
             sfc_alb_dir, source_up, source_dn, source_sfc, flux_dir]
 
     ref_problem_size = (ncol, ngpt)
-    source_kernel_name = f'sw_source_kernel_top_at_1<{str_float}, {top_at_1}, {nlay}>'
-    source_ref_kernel_name = f'sw_source_kernel<{str_float}>'
+    source_kernel_name = f'sw_source_kernel_top_at_1<{common.str_float}, {top_at_1}, {nlay}>'
+    source_ref_kernel_name = f'sw_source_kernel<{common.str_float}>'
 
     two_stream_ref_problem_size = (ncol, nlay, ngpt)
-    two_stream_ref_kernel_name = f'sw_2stream_kernel<{str_float}>'
-    two_stream_kernel_name = f'sw_2stream_kernel<{str_float}>'
+    two_stream_ref_kernel_name = f'sw_2stream_kernel<{common.str_float}>'
+    two_stream_kernel_name = f'sw_2stream_kernel<{common.str_float}>'
 
-    fused_kernel_name = f"sw_source_2stream_kernel<{str_float}, {top_at_1}>"
+    fused_kernel_name = f"sw_source_2stream_kernel<{common.str_float}, {top_at_1}>"
 
 
     if command_line.tune:
