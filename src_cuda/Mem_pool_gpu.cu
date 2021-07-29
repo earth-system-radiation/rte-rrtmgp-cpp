2ab6ca7f486877c1ea8f712db5f2c0e00cf67f77
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-07-29 11:23:11 +0000

    Write size_t as lu instead of i in printf.

--- src_cuda/Mem_pool_gpu.cu
+++ src_cuda/Mem_pool_gpu.cu
@@ -6,15 +6,15 @@ Memory_pool_gpu::Memory_pool_gpu(const std::map<std::size_t, std::size_t>& block
     
     // Preprocess mapping: ensure large enough splitting between block sizes to enhance memory re-use
     std::map<std::size_t, std::size_t> split_block_sizes;
-    if(min_queue_split > 0)
+    if (min_queue_split > 0)
     {
         std::size_t num_ptrs = 0;
-        for(auto it = block_sizes_.begin(); it != block_sizes_.end(); ++it)
+        for (auto it = block_sizes_.begin(); it != block_sizes_.end(); ++it)
         {
             num_ptrs += it->second;
             auto it2 = it;
             ++it2;
-            if(it2 == block_sizes_.end() or it->first < (1. - min_queue_split) * (it2->first))
+            if (it2 == block_sizes_.end() or it->first < (1. - min_queue_split) * (it2->first))
             {
                 split_block_sizes[it->first] = num_ptrs;
                 num_ptrs = 0;
@@ -29,7 +29,7 @@ Memory_pool_gpu::Memory_pool_gpu(const std::map<std::size_t, std::size_t>& block
     std::size_t tot_size = 0;
 
     // First pass: determine big root block size
-    for(auto it = split_block_sizes.begin(); it != split_block_sizes.end(); ++it)
+    for (auto it = split_block_sizes.begin(); it != split_block_sizes.end(); ++it)
     {
         tot_size += (it->first * it->second);
     }
@@ -38,11 +38,11 @@ Memory_pool_gpu::Memory_pool_gpu(const std::map<std::size_t, std::size_t>& block
     
     //Second pass: split block into sub-block pointers
     char* ptr = block;
-    for(auto it = split_block_sizes.begin(); it != split_block_sizes.end(); ++it)
+    for (auto it = split_block_sizes.begin(); it != split_block_sizes.end(); ++it)
     {
         auto block_size = it->first;
         std::list<void*> pointers;
-        for(std::size_t i = 0; i < it->second ; ++i)
+        for (std::size_t i = 0; i < it->second ; ++i)
         {
             pointers.push_back((void*)ptr);
             size_registry[(void*)ptr] = block_size;
@@ -56,7 +56,7 @@ Memory_pool_gpu::Memory_pool_gpu(const std::map<std::size_t, std::size_t>& block
 Memory_pool_gpu::~Memory_pool_gpu()
 {
     // Free raw pointers
-    for(auto it = raw_pointers.begin(); it != raw_pointers.end(); ++it)
+    for (auto it = raw_pointers.begin(); it != raw_pointers.end(); ++it)
     {
         cudaFree(*it);
     }
@@ -71,7 +71,7 @@ void* Memory_pool_gpu::allocate(std::size_t nbytes_)
     int err = cudaMalloc((void **) &data_ptr, nbytes_);
     if (cudaSuccess != err)
     {
-        printf("cudaMalloc failed attempting to allocate %i bytes\n", nbytes_);
+        printf("cudaMalloc failed attempting to allocate %lu bytes\n", nbytes_);
         throw 1;
     }
     alloc_counter++;
@@ -83,7 +83,7 @@ void* Memory_pool_gpu::allocate(std::size_t nbytes_)
 
 void* Memory_pool_gpu::acquire(std::size_t nbytes_)
 {
-    if(nbytes_ == 0)
+    if (nbytes_ == 0)
     {
         return nullptr;
     }
@@ -95,7 +95,7 @@ void* Memory_pool_gpu::acquire(std::size_t nbytes_)
     }
     void* ptr;
     // If the requested block size is too large, add it
-    if(it == blocks.end())
+    if (it == blocks.end())
     {
 //        std::cerr<<"append allocation of size "<<nbytes_<<std::endl;
         ptr = allocate(nbytes_);
@@ -105,14 +105,14 @@ void* Memory_pool_gpu::acquire(std::size_t nbytes_)
     else
     {
         // If the queue is exhausted, look forward for larger available blocks
-        if(it->second.size() == 0)
+        if (it->second.size() == 0)
         {
             auto it2 = it;
             while(it2 != blocks.end() and it2->second.size() == 0)
             {
                 ++it2;
             }
-            if(it2 == blocks.end())
+            if (it2 == blocks.end())
             {
 //                std::cerr<<"insert allocation of size "<<it->first<<", "<<nbytes_<<" requested"<<std::endl;
                 ptr = allocate(it->first);
