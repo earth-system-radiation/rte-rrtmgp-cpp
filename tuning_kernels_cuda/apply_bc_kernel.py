93c48611393e9e825709871f40d9f28001215efb
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2021-09-28 11:37:03 +0200

    Added another kernel of the same family.

--- tuning_kernels_cuda/apply_bc_kernel.py
+++ tuning_kernels_cuda/apply_bc_kernel.py
@@ -17,11 +17,14 @@ def parse_command_line():
     parser.add_argument('--block_size_y_fact', type=int, default=32)
     parser.add_argument('--block_size_x_0', type=int, default=32)
     parser.add_argument('--block_size_y_0', type=int, default=32)
+    parser.add_argument('--block_size_x_lw', type=int, default=64)
+    parser.add_argument('--block_size_y_lw', type=int, default=2)
     return parser.parse_args()
 
 
 # Run one instance of the kernel and test output
 def run_and_test(params: OrderedDict):
+    global inc_flux
     global flux_dn
     global flux_dn_ref
     # increment case
@@ -46,10 +49,20 @@ def run_and_test(params: OrderedDict):
     apply_bc_kernel_0(flux_dn_ref)
     result = kt.run_kernel(kernel_name["0"], kernel_src, problem_size, args, params["0"], compiler_options=common.cp)
     common.compare_fields(result[4], flux_dn_ref, "flux_dn")
+    # lw case
+    print(f"Running apply_BC_kernel_lw [{params['lw']['block_size_x']} {params['lw']['block_size_y']}]")
+    inc_flux = common.random(inc_flux_lw_size, common.type_float)
+    flux_dn = common.random(flux_dn_size, common.type_float)
+    flux_dn_ref = flux_dn
+    args = [isfc, ncol, nlay, ngpt, top_at_1, inc_flux, flux_dn]
+    apply_bc_kernel_lw(flux_dn_ref, inc_flux)
+    result = kt.run_kernel(kernel_name["lw"], kernel_src, problem_size, args, params["lw"], compiler_options=common.cp)
+    common.compare_fields(result[6], flux_dn_ref, "flux_dn")
 
 
 # Tuning
 def tune():
+    global inc_flux
     global flux_dn
     global flux_dn_ref
     tune_params = OrderedDict()
@@ -68,8 +81,8 @@ def tune():
         json.dump(result, fp)
     # factor case
     print(f"Tuning {kernel_name['fact']}")
-    flux_dn = common.zero(flux_dn_size, common.type_float)
-    flux_dn_ref = common.zero(flux_dn_size, common.type_float)
+    flux_dn = common.random(flux_dn_size, common.type_float)
+    flux_dn_ref = flux_dn
     args = [ncol, nlay, ngpt, top_at_1, inc_flux, factor, flux_dn]
     apply_bc_kernel_fact(flux_dn_ref, inc_flux, factor)
     answer = [None for _ in range(0, len(args))]
@@ -80,8 +93,8 @@ def tune():
         json.dump(result, fp)
     # zero case
     print(f"Tuning {kernel_name['0']}")
-    flux_dn = common.zero(flux_dn_size, common.type_float)
-    flux_dn_ref = common.zero(flux_dn_size, common.type_float)
+    flux_dn = common.random(flux_dn_size, common.type_float)
+    flux_dn_ref = flux_dn
     args = [ncol, nlay, ngpt, top_at_1, flux_dn]
     apply_bc_kernel_0(flux_dn_ref)
     answer = [None for _ in range(0, len(args))]
@@ -89,6 +102,19 @@ def tune():
     result, env = kt.tune_kernel(kernel_name["0"], kernel_src, problem_size, args, tune_params, answer=answer,
                                  compiler_options=common.cp, verbose=True, restrictions=restrictions)
     with open("timings_apply_BC_kernel_0.json", "w") as fp:
+        json.dump(result, fp)
+    # lw case
+    print(f"Tuning {kernel_name['lw']}")
+    inc_flux = common.random(inc_flux_lw_size, common.type_float)
+    flux_dn = common.random(flux_dn_size, common.type_float)
+    flux_dn_ref = flux_dn
+    args = [isfc, ncol, nlay, ngpt, top_at_1, inc_flux, flux_dn]
+    apply_bc_kernel_lw(flux_dn_ref, inc_flux)
+    answer = [None for _ in range(0, len(args))]
+    answer[5] = flux_dn_ref
+    result, env = kt.tune_kernel(kernel_name["lw"], kernel_src, problem_size, args, tune_params, answer=answer,
+                                 compiler_options=common.cp, verbose=True, restrictions=restrictions)
+    with open("timings_apply_BC_kernel_inc.json", "w") as fp:
         json.dump(result, fp)
 
 
@@ -123,6 +149,16 @@ def apply_bc_kernel_0(output):
             output[item] = 0.0
 
 
+def apply_bc_kernel_lw(output, inp):
+    for icol in range(0, ncol):
+        for igpt in range(0, ngpt):
+            if top_at_1 == 1:
+                item = (igpt * ncol * (nlay + 1)) + icol
+            else:
+                item = (igpt * ncol * (nlay + 1)) + (nlay * ncol) + icol
+            output[item] = inp[(igpt * ncol * (nlay + 1)) + (isfc * ncol) + icol]
+
+
 if __name__ == '__main__':
     command_line = parse_command_line()
 
@@ -139,8 +175,13 @@ if __name__ == '__main__':
     nlay = common.type_int(140)
     ngpt = common.type_int(224)
     top_at_1 = common.type_bool(1)
+    if top_at_1 == 1:
+        isfc = common.type_int(0)
+    else:
+        isfc = common.type_int(nlay)
     flux_dn_size = ncol * (nlay + 1) * ngpt
     inc_flux_size = ncol * ngpt
+    inc_flux_lw_size = ncol * nlay * ngpt
     inc_flux = common.random(inc_flux_size, common.type_float)
     factor = common.random(ncol, common.type_float)
     # Output
