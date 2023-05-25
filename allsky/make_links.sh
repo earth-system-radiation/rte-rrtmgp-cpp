dcc0c0160326b6ab5111c6acda6b807fbefdcb3e
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2023-05-25 11:35:34 +0200

    Add modified check script to avoid comparing aerosol columns for all sky.

--- allsky/make_links.sh
+++ allsky/make_links.sh
@@ -4,5 +4,4 @@ ln -sf ../rte-rrtmgp/rrtmgp-data/rrtmgp-cloud-optics-coeffs-lw.nc cloud_coeffici
 ln -sf ../rte-rrtmgp/rrtmgp-data/rrtmgp-data-sw-g224-2018-12-04.nc coefficients_sw.nc
 ln -sf ../rte-rrtmgp/rrtmgp-data/rrtmgp-data-lw-g256-2018-12-04.nc coefficients_lw.nc
 ln -sf ../rte-rrtmgp/examples/all-sky/garand-atmos-1.nc .
-ln -sf ../rte-rrtmgp/examples/all-sky/compare-to-reference.py .
 ln -sf ../build/test_rte_rrtmgp .
