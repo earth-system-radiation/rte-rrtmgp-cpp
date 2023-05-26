a86adf5933f6a6a8b0389b1217a1f6c776525440
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2023-05-26 11:03:35 +0200

    Update ubuntu config to 22 LTS

---
+++ config/ubuntu_22lts.cmake
@@ -0,0 +1,35 @@
+# Ubuntu 20.04
+if(USEMPI) 
+  set(ENV{CC}  mpicc ) # C compiler for parallel build
+  set(ENV{CXX} mpicxx) # C++ compiler for parallel build
+else()
+  set(ENV{CC}  gcc) # C compiler for serial build
+  set(ENV{CXX} g++) # C++ compiler for serial build
+endif()
+
+set(USER_CXX_FLAGS "-std=c++17")
+set(USER_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -march=native")
+set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+set(USER_FC_FLAGS "-fdefault-real-8 -fdefault-double-8 -fPIC -ffixed-line-length-none -fno-range-check")
+set(USER_FC_FLAGS_RELEASE "-DNDEBUG -O3 -march=native")
+set(USER_FC_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+set(NETCDF_INCLUDE_DIR "/usr/include")
+set(NETCDF_LIB_C       "/usr/lib/x86_64-linux-gnu/libnetcdf.so")
+set(HDF5_LIB_1         "/usr/lib/x86_64-linux-gnu/libhdf5_serial.so")
+set(HDF5_LIB_2         "/usr/lib/x86_64-linux-gnu/libhdf5_serial_hl.so")
+set(SZIP_LIB           "")
+set(LIBS ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
+set(INCLUDE_DIRS ${NETCDF_INCLUDE_DIR})
+
+if(USECUDA)
+  set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+  set(CMAKE_CUDA_ARCHITECTURES 80)
+  set(USER_CUDA_FLAGS "-std=c++17 -expt-relaxed-constexpr")
+  set(USER_CUDA_FLAGS_RELEASE "-Xptxas -O3 -DNDEBUG")
+  set(USER_CUDA_FLAGS_DEBUG "-Xptxas -O0 -g -G -DCUDACHECKS")
+  # add_definitions(-DRRTMGP_RTE_GPU_MEMPOOL_OWN)
+  add_definitions(-DRRTMGP_RTE_GPU_MEMPOOL_CUDA)
+endif()
+
+add_definitions(-DRTE_USE_CBOOL)
